45bb595a1f9d92e6dbaa23d080421edaf7acba78
Mike Ludemann <mike-ludemann@hotmail.de>
2018-05-18 15:48:02 +0200

    Removing code, that does not use

--- src/socialMedia.js
+++ src/socialMedia.js
@@ -195,14 +195,6 @@
             $("body").css("position", "");
         });
 
-        return this.css({
-            title: settings.title,
-            content: settings.content,
-            footer: settings.footer,
-            radiusBorder: settings.radiusBorder,
-            fontSize: settings.fontSize
-        });
-
     };
 
 }(jQuery));
